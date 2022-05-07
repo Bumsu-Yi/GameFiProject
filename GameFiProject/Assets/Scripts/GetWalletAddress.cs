9feba89917bced337a3339f76812e484ac6e9c2e
cartoonweber <minkbuckstar@gmail.com>
2022-05-07 13:02:22 +0800

    metamask connect

---
+++ GameFiProject/Assets/Scripts/GetWalletAddress.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+// use web3.jslib
+using System.Runtime.InteropServices;
+
+public class GetWalletAddress : MonoBehaviour
+{
+  // text in the button
+  public Text ButtonText;
+  // use WalletAddress function from web3.jslib
+  [DllImport("__Internal")] private static extern string WalletAddress();
+
+  void Start()
+  {
+        
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+  }
+  
+  public void OnClick()
+  {
+    // get wallet address and display it on the button
+    ButtonText.text = WalletAddress();
+  }
+}
